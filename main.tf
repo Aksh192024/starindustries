00c8f6a6db37343d5c0e5bb4e16d3afcb02d918a
Aksh192024 <Akshnegi1924@gmail.com>
2026-01-22 18:32:55 +0530

    adding vpc

--- main.tf
+++ main.tf
@@ -1,3 +1,28 @@
 module "vpc" {
-  source = "./modules/vpc"
+  source = "git::https://github.com/Aksh192024/Terraform_modules.git//vpc"
+
+  vpc_cidr = var.vpc_cidr
+  vpc_name = var.vpc_name
+}
+
+#subnet
+
+# Public Subnet
+module "public_subnet" {
+  source            = "git::https://github.com/Aksh192024/Terraform_modules.git//subnet"
+  vpc_id            = data.terraform_remote_state.vpc.outputs.vpc_id
+  cidr_block        = var.public_subnet_cidr
+  availability_zone = var.availability_zone
+  is_public         = true
+  subnet_name       = var.public_subnet_name
+}
+
+# Private Subnet
+module "private_subnet" {
+  source            = "git::https://github.com/Aksh192024/Terraform_modules.git//subnet"
+  vpc_id            = data.terraform_remote_state.vpc.outputs.vpc_id
+  cidr_block        = var.private_subnet_cidr
+  availability_zone = var.availability_zone
+  is_public         = false
+  subnet_name       = var.private_subnet_name
 }
